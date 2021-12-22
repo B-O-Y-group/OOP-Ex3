1c0a52494822235a24522c18940bcbec0585e22f
yosefkahlon <yosefkahlon53@gmail.com>
2021-12-22 19:52:38 +0200

    load and save

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -20,33 +20,35 @@ class GraphAlgo(GraphAlgoInterface, ABC):
     ##    add_node(self, node_id: int, pos: tuple = None)
 
     def load_from_json(self, file_name: str) -> bool:
-        dic = {}
+        try:
+            dic = {}
+            g = DiGraph()
+            with open(file_name, "r") as f:
+                dic = json.load(fp=f)
+            for n in dic["Nodes"].values():
+                ##p = n["pos"].values()
+                ## pos = tuple(map(float, pos.split(",")))
+                g.add_node(node_id=n["id"], pos=(n["pos"]))
 
-        with open(file_name, "r") as f:
-            dic = json.load(fp=f)
-        for n in dic["Nodes"].values():
-            ##p = n["pos"].values()
-            ## pos = tuple(map(float, pos.split(",")))
-            self.graph.add_node(node_id=n["id"], pos=n["pos"])
+            for e in dic["Edges"].values():
+                g.add_edge(id1=["src"], id2=["dest"], weight=["w"])
 
-        for e in dic["Edges"].values():
-            self.graph.add_edge(id1=["src"], id2=["dest"], weight=["w"])
-
-            if self.graph == None:
-                return False
-        return True
+                self.graph = g
+            return True
+        except Exception:
+            return False
 
     def save_to_json(self, file_name: str) -> bool:
-        with open(file_name, "w") as f:
-            ## indenter is number rof space
-            ## default= lambda  o :o.__dict__ taking the self and change him to dictionaries
-            json.dump(self, filePointer=f, indent=4, default=lambda o: o.__dict__)
-        return True
-
+        try:
+            with open(file_name, "w") as f:
+                ## indenter is number rof space
+                ## default= lambda  o :o.__dict__ taking the self and change him to dictionaries
+                json.dump(self, filePointer=f, indent=4, default=lambda o: o.__dict__)
+            return True
+        except Exception:
+            return False
 
     def shortest_path(self, id1: int, id2: int) -> (float, list):
-
-        
 
     def plot_graph(self) -> None:
         pass
@@ -59,8 +61,12 @@ if __name__ == '__main__':
     g.add_node(1, pos)
     g.add_node(2, pos)
 
-    print(g)
+    # print(g)
 
     g.add_edge(1, 2, 0)
     g.add_edge(2, 1, 0)
-    print(g)
+    # print(g)
+
+    algo = GraphAlgo(g)
+
+    print(algo.get_graph())
